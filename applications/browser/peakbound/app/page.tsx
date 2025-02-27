e0cd6eb15da4cc81e8a7d8e0eca374db534ff753
jake-the-cake <askjake331@gmail.com>
2025-02-27 16:22:41 -0500

    logo adjustment

--- applications/browser/peakbound/app/page.tsx
+++ applications/browser/peakbound/app/page.tsx
@@ -1,20 +1,8 @@
-'use client';
-
-// import Image from "next/image"
-// import { FormEvent } from "react"
-// import { RequestBody } from "shared/request"
-
 export default function Home() {
 
-	// function handleSubmit(e: FormEvent<HTMLFormElement>) {
-	// 	e.preventDefault()
-	// 	const request = new RequestBody(new FormData(e.target as HTMLFormElement))
-	// }
-
   return (
-		<div className="flex">
-			<a href="">something</a>
-			<h1>something</h1>
-		</div>
-  );
+		<main>
+			
+		</main>
+	);
 }
