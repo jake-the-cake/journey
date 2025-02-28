4e479d04247014916cad2a7991c74e1805b4c739
jake-the-cake <askjake331@gmail.com>
2025-02-28 13:22:15 -0500

    menu dropdown

--- applications/browser/peakbound/app/_components/Navbar.tsx
+++ applications/browser/peakbound/app/_components/Navbar.tsx
@@ -18,6 +18,23 @@ export default function Navbar() {
 				<div className="menu-bar"></div>
 				<div className="menu-bar"></div>
 			</div>
+			<ul className="menu">
+				<div className="menu-mask"></div>
+				<li>
+					<div className="menu-category">Learn More</div>
+					<div className="menu-category-items">
+						<div>Our Mission</div>
+						<div>Comany History</div>
+					</div>
+				</li>
+				<li>
+					<div className="menu-category">Learn More</div>
+					<div className="menu-category-items">
+						<div>One</div>
+						<div>Two</div>
+					</div>
+				</li>
+			</ul>
 		</nav>
 	)
 }
--- applications/browser/peakbound/app/page.tsx
+++ applications/browser/peakbound/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
 
   return (
 		<main>
-			
+			<h1>Want More?</h1>
 		</main>
 	);
 }
