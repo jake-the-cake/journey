1e68a74d308a7fe1f2844e630bed99a249bb1898
jake-the-cake <askjake331@gmail.com>
2025-03-01 20:59:39 -0500

    dropdown

--- applications/browser/peakbound/app/_components/Navbar.tsx
+++ applications/browser/peakbound/app/_components/Navbar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { MouseEvent } from "react"
+import { EventHandler, MouseEvent } from "react"
 
 export default function Navbar() {
 	
@@ -8,10 +8,39 @@ export default function Navbar() {
 		console.log("Menu clicked")
 	}
 
+	function getCategoryList(target: HTMLDivElement): HTMLDivElement | void {
+		console.log(target)
+		const parent = target.parentNode
+		if (!parent) throw new Error('Missing navbar element.')
+		const list = parent!.querySelector('.menu-category-items')
+		if (list) return list as HTMLDivElement
+	}
+
+
+	function handleExpandClose(list: HTMLDivElement): (e: any) => void {
+		return function(e: MouseEvent<HTMLDivElement>) {
+			if (list && !list.contains(e.currentTarget)) {
+				list.classList.remove('expanded-menu-category')
+				console.log(list)
+			}
+		}
+	}
+
+	function handleExpand(e: MouseEvent<HTMLDivElement>) {
+		const list = getCategoryList(e.currentTarget)
+		if (list) {
+			list?.classList.add('expanded-menu-category')
+			console.log(list)
+			document.addEventListener('click', handleExpandClose(list) as any, { once: true })
+		}
+	}
+
 	return (
 		<nav>
 			<div className="logo">
-				Peak Bound Journeys
+				<a href="/">
+					Peak Bound Journeys
+				</a>
 			</div>
 			<div className="menu-icon" onClick={ handleMenuClick }>
 				<div className="menu-bar"></div>
@@ -21,14 +50,14 @@ export default function Navbar() {
 			<ul className="menu">
 				<div className="menu-mask"></div>
 				<li>
-					<div className="menu-category">Learn More</div>
+					<div className="menu-category" onClick={ handleExpand }>Learn More</div>
 					<div className="menu-category-items">
-						<div>Our Mission</div>
-						<div>Comany History</div>
+						<div><a href="mission">Our Mission</a></div>
+						<div>Company History</div>
 					</div>
 				</li>
 				<li>
-					<div className="menu-category">Learn More</div>
+					<div className="menu-category" onClick={ handleExpand }>Learn More</div>
 					<div className="menu-category-items">
 						<div>One</div>
 						<div>Two</div>
