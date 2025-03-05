b77cd22624667d44248a16da160f302fb7e45c0c
jake-the-cake <askjake331@gmail.com>
2025-03-05 15:52:31 -0500

    pyramid building

--- applications/browser/peakbound/app/page.tsx
+++ applications/browser/peakbound/app/page.tsx
@@ -3,13 +3,15 @@ export default function Home() {
   return (
 		<main>
 			<section className="accent">
-				<div className="pyramid-list">
-					<h2 className="">Recover</h2>
-					<h2>Rebuild</h2>
-					<h2>Redeem</h2>
+				<div className="pyramid-container">
+					<div className="pyramid-list">
+						<h2 className="">Recover</h2>
+						<h2>Rebuild</h2>
+						<h2>Redeem</h2>
+					</div>
 				</div>
+				<h1>It's Time To Be YOU</h1>
 			</section>
-			<h1>It's Time To Be YOU</h1>
 		</main>
 	);
 }
