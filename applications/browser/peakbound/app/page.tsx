3825381ea68361a1f71a342d393d23228877b82e
jake-the-cake <askjake331@gmail.com>
2025-03-06 15:11:24 -0500

    cards dropping down

--- applications/browser/peakbound/app/page.tsx
+++ applications/browser/peakbound/app/page.tsx
@@ -1,16 +1,43 @@
+'use client';
+
+import { useEffect } from 'react'
+
 export default function Home() {
+
+	useEffect(() => {
+		const p: any = document.querySelector('.pyramid-list')
+		console.log(p)
+		if (!p) return
+		const cards: HTMLElement[] = Array.from(p.children)
+		return dropCard(0)
+		function dropCard(index: number = 0): void {
+			cards[index].classList.add('dropped')
+			console.log(index)
+			index++
+			if (cards.length > index) {
+				setTimeout(() => {
+					dropCard(index)
+				}, 1500)
+			}
+		}
+		// Array.from(p).forEach((item, index, array) => {
+
+		// })
+	}, [])
 
   return (
 		<main>
-			<section className="accent">
+			<section className="full nature">
+				<h1 className='hero'>Who Are YOU?</h1>
+				<p></p>
 				<div className="pyramid-container">
+					<button className="">Create Your New Path</button>
 					<div className="pyramid-list">
 						<h2 className="">Recover</h2>
 						<h2>Rebuild</h2>
 						<h2>Redeem</h2>
 					</div>
 				</div>
-				<h1>It's Time To Be YOU</h1>
 			</section>
 		</main>
 	);
