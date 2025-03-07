0501fa8d82f5be5b11fed5323dc0d83b29b646e7
jake-the-cake <askjake331@gmail.com>
2025-03-06 19:02:15 -0500

    setting up hero section

--- applications/browser/peakbound/app/page.tsx
+++ applications/browser/peakbound/app/page.tsx
@@ -1,41 +1,53 @@
 'use client';
 
 import { useEffect } from 'react'
+import internal from 'stream';
 
 export default function Home() {
 
 	useEffect(() => {
-		const p: any = document.querySelector('.pyramid-list')
-		console.log(p)
+		const p: HTMLElement | null = document.querySelector('.pyramid-list')
 		if (!p) return
-		const cards: HTMLElement[] = Array.from(p.children)
+		
+		const cards: Element[] = Array.from(p.children)
 		return dropCard(0)
+		
+		function resetCards(): void {
+			cards.forEach((card: Element) => {
+				card.classList.remove('dropped')
+			})
+			useTimeout(dropCard, 0)
+		}
+
 		function dropCard(index: number = 0): void {
 			cards[index].classList.add('dropped')
-			console.log(index)
 			index++
-			if (cards.length > index) {
-				setTimeout(() => {
-					dropCard(index)
-				}, 1500)
-			}
+			if (cards.length > index) useTimeout(dropCard, index)
+			else useTimeout(resetCards)
 		}
-		// Array.from(p).forEach((item, index, array) => {
 
-		// })
+		type argFunction = (args: any) => void
+
+		function useTimeout(callback: () => void | argFunction, ...args: any) {
+			setTimeout(() => {
+				callback(...args)
+			}, 1500)
+		}
 	}, [])
 
   return (
 		<main>
 			<section className="full secondary">
-				<h1 className='hero'>Who Are You?</h1>
-				<p className='hero'>
-					More importantly... who do you want to be?
-				</p>
+				<div className="hero-head">
+					<h1 className='hero'>Who Are You?</h1>
+					<p className='hero'>
+						More importantly... who do you <span className="bold">want</span> to be?
+					</p>
+				</div>
 			</section>
 			<section className="full nature">
 				<div className="pyramid-container">
-					<button>Create Your New Path</button>
+					<button>Start On Your Path</button>
 					<div className="pyramid-list">
 						<h2>Recover</h2>
 						<h2>Rebuild</h2>
