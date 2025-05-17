55055d7afb791e2f9d5ff016b96fa3c1cf9777d0
jake-the-cake <askjake331@gmail.com>
2025-05-17 18:32:50 -0400

    refactor: remove DreamBox component and clean up Home page

--- applications/browser/peakbound/app/_components/Navbar.tsx
+++ applications/browser/peakbound/app/_components/Navbar.tsx
@@ -1,13 +1,22 @@
 'use client';
 
-import { MouseEvent } from "react"
 import Link from "next/link"
 
 export default function Navbar() {
 
 	return (
 		<nav>
-			<img src="peakboundmountains.png" className="main-logo" alt="" />
+			<div className="main-logo">
+				<img src="peakboundmountains.png" className="main-logo" alt="" />
+			</div>
+			<div className="menu-bar">
+				<ul>
+					<li><Link href={ '/schedule'}>Schedule</Link></li>
+					<li><Link href={ '/programs'}>Programs</Link></li>
+					<li><Link href={ '/aboutus'}>About Us</Link></li>
+					<li><Link href={ '/donate'}>Donate</Link></li>
+				</ul>
+			</div>
 		</nav>
 	)
 }
