ae56bb774d99bb407146ba1481415eb99e2fb29f
jake-the-cake <askjake331@gmail.com>
2025-05-18 16:26:55 -0400

    dynamic changing navbar script added

--- applications/browser/peakbound/app/_components/Navbar.tsx
+++ applications/browser/peakbound/app/_components/Navbar.tsx
@@ -1,22 +1,49 @@
 'use client';
 
 import Link from "next/link"
+import Script from "next/script";
 
 export default function Navbar() {
 
 	return (
+		<>
 		<nav>
-			<div className="main-logo">
-				<img src="peakboundmountains.png" className="main-logo" alt="" />
+			<div className="main-logo-mask">
+				<Link href="/">
+					<img src="peakboundmountains.png" className="main-logo" alt="" />
+				</Link>
 			</div>
 			<div className="menu-bar">
 				<ul>
-					<li><Link href={ '/schedule'}>Schedule</Link></li>
-					<li><Link href={ '/programs'}>Programs</Link></li>
-					<li><Link href={ '/aboutus'}>About Us</Link></li>
-					<li><Link href={ '/donate'}>Donate</Link></li>
+					<li><Link href={ '/schedule' }>Schedule</Link></li>
+					<li><Link href={ '/programs' }>Programs</Link></li>
+					<li><Link href={ '/aboutus' }>About Us</Link></li>
+					<li><Link href={ '/donate' }>Donate</Link></li>
 				</ul>
 			</div>
 		</nav>
+		<Script id="nav-scroll-handler" strategy="lazyOnload">
+			{`
+				const nav = document.querySelector('nav')
+				const main = document.querySelector('main')
+				const className = 'nav-h'
+				if (nav && main) {
+					let triggerHeight = nav.offsetHeight
+					window.addEventListener('scroll', function () {
+					if (window.scrollY > triggerHeight && !nav.classList.contains(className)) {
+						nav.classList.add(className)
+						triggerHeight = nav.offsetHeight
+						main.style.marginTop = triggerHeight + 50 + 'px'
+					}
+					if (window.scrollY <= triggerHeight && nav.classList.contains(className)) {
+						nav.classList.remove(className)
+						triggerHeight = nav.offsetHeight
+						main.style.marginTop = '0px'
+					}
+					})
+				}
+			`}
+		</Script>
+		</>
 	)
 }
